4f3116f7fecd6997429449ab82f876839c115cab
Moky <albert.moky@gmail.com>
2020-02-07 19:01:33 +0800

    SessionStorage

--- StarGate/src/storage.js
+++ StarGate/src/storage.js
@@ -36,122 +36,152 @@
 !function (ns) {
     "use strict";
 
-    var Storage = {
-
-        ROOT: 'dim.fs',
-
-        //
-        //  Read
-        //
-
-        /**
-         *  Check whether data file exists
-         *
-         * @param path
-         * @returns {boolean}
-         */
-        exists: function (path) {
-            return !!this.loadText(path);
-        },
-
-        /**
-         *  Load text from file path
-         *
-         * @param path
-         * @returns {string}
-         */
-        loadText: function (path) {
-            return this.storage.getItem(this.ROOT + '.' + path);
-        },
-        /**
-         *  Load data from file path
-         *
-         * @param path
-         * @returns {*[]}
-         */
-        loadData: function (path) {
-            var base64 = this.loadText(path);
-            if (!base64) {
-                return null;
-            }
-            return DIMP.format.Base64.decode(base64);
-        },
-        /**
-         *  Load JSON from file path
-         *
-         * @param path
-         * @returns {Dictionary|Array}
-         */
-        loadJSON: function (path) {
-            var json = this.loadText(path);
-            if (!json) {
-                return null;
-            }
-            return DIMP.format.JSON.decode(json);
-        },
-
-        //
-        //  Write
-        //
-
-        /**
-         *  Delete file
-         *
-         * @param path
-         */
-        remove: function (path) {
-            this.saveText(null, path);
-            return true;
-        },
-
-        /**
-         *  Save string into Text file
-         *
-         * @param text
-         * @param path
-         */
-        saveText: function (text, path) {
-            if (text) {
-                this.storage.setItem(this.ROOT + '.' + path, text);
-                return true;
-            } else {
-                this.storage.removeItem(this.ROOT + '.' + path);
-                return false;
-            }
-        },
-        /**
-         *  Save data into binary file
-         *
-         * @param data
-         * @param path
-         */
-        saveData: function (data, path) {
-            var base64 = null;
-            if (data) {
-                base64 = DIMP.format.Base64.encode(data);
-            }
-            return this.saveText(base64, path);
-        },
-        /**
-         *  Save Map/List into JSON file
-         *
-         * @param container
-         * @param path
-         */
-        saveJSON: function (container, path) {
-            var json = null;
-            if (container) {
-                json = DIMP.format.JSON.encode(container);
-            }
-            return this.saveText(json, path);
+    var Storage = function (storage, prefix) {
+        // Web storage
+        this.storage = storage;
+        // key prefix
+        if (prefix) {
+            this.ROOT = prefix;
+        } else {
+            this.ROOT = 'dim';
         }
     };
 
-    Storage.storage = window.localStorage;
+    Storage.prototype.getItem = function (key) {
+        return this.storage.getItem(key);
+    };
+    Storage.prototype.setItem = function (key, value) {
+        this.storage.setItem(key, value);
+    };
+    Storage.prototype.removeItem = function (key) {
+        return this.storage.removeItem(key);
+    };
+    Storage.prototype.clear = function () {
+        return this.storage.clear();
+    };
+
+    Storage.prototype.getLength = function () {
+        return this.storage.length;
+    };
+    Storage.prototype.key = function (index) {
+        return this.storage.key(index);
+    };
+
+    //
+    //  Read
+    //
+
+    /**
+     *  Check whether data file exists
+     *
+     * @param path
+     * @returns {boolean}
+     */
+    Storage.prototype.exists = function (path) {
+        return !!this.getItem(this.ROOT + '.' + path);
+    };
+
+    /**
+     *  Load text from file path
+     *
+     * @param path
+     * @returns {string}
+     */
+    Storage.prototype.loadText = function (path) {
+        return this.getItem(this.ROOT + '.' + path);
+    };
+
+    /**
+     *  Load data from file path
+     *
+     * @param path
+     * @returns {*[]}
+     */
+    Storage.prototype.loadData = function (path) {
+        var base64 = this.loadText(path);
+        if (!base64) {
+            return null;
+        }
+        return DIMP.format.Base64.decode(base64);
+    };
+
+    /**
+     *  Load JSON from file path
+     *
+     * @param path
+     * @returns {Dictionary|Array}
+     */
+    Storage.prototype.loadJSON = function (path) {
+        var json = this.loadText(path);
+        if (!json) {
+            return null;
+        }
+        return DIMP.format.JSON.decode(json);
+    };
+
+    //
+    //  Write
+    //
+
+    /**
+     *  Delete file
+     *
+     * @param path
+     */
+    Storage.prototype.remove = function (path) {
+        this.removeItem(this.ROOT + '.' + path);
+        return true;
+    };
+
+    /**
+     *  Save string into Text file
+     *
+     * @param text
+     * @param path
+     */
+    Storage.prototype.saveText = function (text, path) {
+        if (text) {
+            this.setItem(this.ROOT + '.' + path, text);
+            return true;
+        } else {
+            this.removeItem(this.ROOT + '.' + path);
+            return false;
+        }
+    };
+
+    /**
+     *  Save data into binary file
+     *
+     * @param data
+     * @param path
+     */
+    Storage.prototype.saveData = function (data, path) {
+        var base64 = null;
+        if (data) {
+            base64 = DIMP.format.Base64.encode(data);
+        }
+        return this.saveText(base64, path);
+    };
+
+    /**
+     *  Save Map/List into JSON file
+     *
+     * @param container
+     * @param path
+     */
+    Storage.prototype.saveJSON = function (container, path) {
+        var json = null;
+        if (container) {
+            json = DIMP.format.JSON.encode(container);
+        }
+        return this.saveText(json, path);
+    };
 
     //-------- namespace --------
-    ns.LocalStorage = Storage;
+    ns.LocalStorage = new Storage(window.localStorage, 'dim.fs');
+    ns.SessionStorage = new Storage(window.sessionStorage, 'dim.mem');
 
     ns.register('LocalStorage');
+    ns.register('SessionStorage');
 
 }(StarGate);
